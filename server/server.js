2d678ba05b2ef3fa2117be8771420b137d17f512
Jen <jen.lijo@zalando.de>
2018-04-26 17:45:09 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,16 +9,26 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.post('/todos', (req, res) => {
+app.post('/todos', async (req, res) => {
   const todo = new Todo({
     text: req.body.text
   })
 
-  todo.save().then((doc) => {
-    res.status(200).send(doc)
-  }, (e) => {
+  try {
+    const todoRecord = await todo.save()
+    res.status(200).send(todoRecord)
+  } catch(e) {
     res.status(400).send(e)
-  })
+  }
+})
+
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find()
+    res.send({ todos })
+  } catch(e) {
+    res.status(400).send(e)
+  }
 })
 
 

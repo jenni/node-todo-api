5654e8235326a9c18452ee2dd5f2237f5291a818
Jen <jen.lijo@zalando.de>
2018-05-26 08:28:01 +0200

    Create User model and email validation

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,34 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: (value) => {
+        return validator.isEmail(value)
+      },
+      message: '{ VALUE } is not a valid email'
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6
+    },
+    tokens: [{
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }]
   }
 })
 

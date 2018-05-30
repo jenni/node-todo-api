caf6299f2603384bdac664340ef5f0f003bbdc7d
Jen <jen.lijo@zalando.de>
2018-05-30 18:33:16 +0200

    Fix UserSchema: Wrap email elements properly

--- server/models/user.js
+++ server/models/user.js
@@ -10,27 +10,25 @@ const UserSchema = new mongoose.Schema({
     minlength: 1,
     unique: true,
     validate: {
-      validator: (value) => {
-        return validator.isEmail(value)
-      },
+      validator: validator.isEmail,
       message: '{ VALUE } is not a valid email'
     },
-    password: {
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
       type: String,
-      required: true,
-      minlength: 6
+      required: true
     },
-    tokens: [{
-      access: {
-        type: String,
-        required: true
-      },
-      token: {
-        type: String,
-        required: true
-      }
-    }]
-  }
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
 UserSchema.methods.generateAuthToken = function() {
